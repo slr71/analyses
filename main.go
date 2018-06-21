4e8f9326015434aa2e10be4af1a2a4eeda9e6b21
John Wregglesworth <wregglej@iplantcollaborative.org>
2018-06-21 09:57:00 -0700

    Update analyses to perform lookups based on job status

--- main.go
+++ main.go
@@ -1,14 +1,76 @@
 package main
 
 import (
+	"context"
 	"database/sql"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 
+	_ "expvar"
+
+	_ "github.com/lib/pq"
+
+	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
 )
+
+// Job is an entry from the jobs table in the database. It contains a minimal
+// set of fields.
+type Job struct {
+	ID             string
+	AppID          string
+	UserID         string
+	Status         string
+	PlannedEndDate int64
+}
+
+// JobList is a list of Jobs. Duh.
+type JobList struct {
+	Jobs []Job
+}
+
+const listJobsQuery = `
+SELECT id,
+       app_id,
+       user_id,
+       status,
+       planned_end_date
+  FROM jobs
+ WHERE status = $1
+   AND planned_end_date < NOW()`
+
+func listJobs(ctx context.Context, db *sql.DB, status string) (*JobList, error) {
+	rows, err := db.QueryContext(ctx, listJobsQuery, status)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	jl := &JobList{
+		Jobs: []Job{},
+	}
+
+	for rows.Next() {
+		var j Job
+		err = rows.Scan(
+			&j.ID,
+			&j.AppID,
+			&j.UserID,
+			&j.Status,
+			&j.PlannedEndDate,
+		)
+		if err != nil {
+			return nil, err
+		}
+		jl.Jobs = append(jl.Jobs, j)
+	}
+	return jl, nil
+}
 
 func main() {
 	var (
@@ -18,6 +80,7 @@ func main() {
 		sslKey     = flag.String("ssl-key", "", "Path to the SSL .key file.")
 	)
 
+	ctx := context.Background()
 	flag.Parse()
 
 	useSSL := false
@@ -42,6 +105,16 @@ func main() {
 	}
 
 	dbURI := viper.GetString("db.uri")
+	dbparsed, err := url.Parse(dbURI)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if dbparsed.Scheme == "postgresql" {
+		dbparsed.Scheme = "postgres"
+	}
+	dbURI = dbparsed.String()
+
+	log.Printf("database url %s\n", dbURI)
 
 	db, err := sql.Open("postgres", dbURI)
 	if err != nil {
@@ -52,15 +125,42 @@ func main() {
 		log.Fatal(err)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	router := mux.NewRouter()
 
-	})
+	router.HandleFunc("/analyses/expired/{status}", func(w http.ResponseWriter, r *http.Request) {
+		validStatuses := map[string]bool{
+			"Completed": true,
+			"Failed":    true,
+			"Submitted": true,
+			"Queued":    true,
+			"Running":   true,
+		}
+
+		vars := mux.Vars(r)
+		status := strings.Title(strings.ToLower(vars["status"]))
+		if _, ok := validStatuses[status]; !ok {
+			http.Error(w, fmt.Sprintf("unknown status %s", status), http.StatusBadRequest)
+			return
+		}
+
+		list, err := listJobs(ctx, db, status)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set(http.CanonicalHeaderKey("content-type"), "application/json")
+
+		json.NewEncoder(w).Encode(list)
+	}).Methods("GET")
+
+	router.Handle("/debug/vars", http.DefaultServeMux)
 
 	addr := fmt.Sprintf(":%d", *listenPort)
 	if useSSL {
-		log.Fatal(http.ListenAndServeTLS(addr, *sslCert, *sslKey, nil))
+		log.Fatal(http.ListenAndServeTLS(addr, *sslCert, *sslKey, router))
 	} else {
-		log.Fatal(http.ListenAndServe(addr, nil))
+		log.Fatal(http.ListenAndServe(addr, router))
 	}
 
 }
