27a8df0b1dad76f83f6743aec01460f0291df691
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-02-27 09:13:38 -0700

    Nuked Go implementation

--- README.md
+++
@@ -1,4 +0,0 @@
-analyses
-========
-
-Returns a list of all of the running analyses from the database.
--- main_test.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "testing"
-
-// Get our CI to shut up.
-func TestNothing(t *testing.T) {
-	return
-}
