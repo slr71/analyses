2101fd153ad612e6c72a0d173b887fb61f5fc362
John Wregglesworth <wregglej@cyverse.org>
2019-02-28 07:41:55 -0700

    Fix indentation in project.clj

--- project.clj
+++ project.clj
@@ -1,34 +1,34 @@
 (defproject analyses "0.1.0-SNAPSHOT"
-            :description "Backend service providing API access to DE analyses."
-            :url "https://github.com/cyverse-de/analyses"
-            :license {:name "BSD 3-Clause"
-                      :url "https://github.com/cyverse-de/analyses/blob/master/LICENSE"}
-            :dependencies [[org.clojure/clojure "1.10.0"]
-                           [clj-http "3.9.1"]
-                           [lambdaisland/uri "1.1.0"]
-                           [medley "1.1.0"]
-                           [compojure "1.6.1"]
-                           [metosin/compojure-api "1.1.11"]
-                           [org.cyverse/common-cli "2.8.1"]
-                           [org.cyverse/common-cfg "2.8.1"]
-                           [org.cyverse/common-swagger-api "2.9.3-SNAPSHOT"]
-                           [org.cyverse/cyverse-groups-client "0.1.5"]
-                           [org.cyverse/service-logging "2.8.0"]
-                           [me.raynes/fs "1.4.6"]
-                           [ring "1.7.1"]
-                           [proto-repl "0.3.1"]]
-            :eastwood {:exclude-namespaces [apps.protocols :test-paths]
-                       :linters [:wrong-arity :wrong-ns-form :wrong-pre-post :wrong-tag :misplaced-docstrings]}
-            :plugins [[lein-swank "1.4.4"]
-                      [test2junit "1.1.3"]
-                      [jonase/eastwood "0.2.6"]]
-            :profiles {:dev {:dependencies   [[ring "1.7.1"]]
-                             :plugins        [[lein-ring "0.12.5"]]
-                             :resource-paths ["conf/test"]}
-                       :repl {:source-paths ["repl"]}
-                       :uberjar {:aot :all}}
-            :main ^:skip-aot analyses.core
-            :ring {:handler analyses.routes/app
-                   :init analyses.core/load-config-from-file
-                   :port 31327}
-            :repl-options {:init-ns analyses.core})
+  :description "Backend service providing API access to DE analyses."
+  :url "https://github.com/cyverse-de/analyses"
+  :license {:name "BSD 3-Clause"
+            :url "https://github.com/cyverse-de/analyses/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-http "3.9.1"]
+                 [lambdaisland/uri "1.1.0"]
+                 [medley "1.1.0"]
+                 [compojure "1.6.1"]
+                 [metosin/compojure-api "1.1.11"]
+                 [org.cyverse/common-cli "2.8.1"]
+                 [org.cyverse/common-cfg "2.8.1"]
+                 [org.cyverse/common-swagger-api "2.9.3-SNAPSHOT"]
+                 [org.cyverse/cyverse-groups-client "0.1.5"]
+                 [org.cyverse/service-logging "2.8.0"]
+                 [me.raynes/fs "1.4.6"]
+                 [ring "1.7.1"]
+                 [proto-repl "0.3.1"]]
+  :eastwood {:exclude-namespaces [apps.protocols :test-paths]
+             :linters [:wrong-arity :wrong-ns-form :wrong-pre-post :wrong-tag :misplaced-docstrings]}
+  :plugins [[lein-swank "1.4.4"]
+            [test2junit "1.1.3"]
+            [jonase/eastwood "0.2.6"]]
+  :profiles {:dev {:dependencies   [[ring "1.7.1"]]
+                   :plugins        [[lein-ring "0.12.5"]]
+                   :resource-paths ["conf/test"]}
+             :repl {:source-paths ["repl"]}
+             :uberjar {:aot :all}}
+  :main ^:skip-aot analyses.core
+  :ring {:handler analyses.routes/app
+         :init analyses.core/load-config-from-file
+         :port 31327}
+  :repl-options {:init-ns analyses.core})
