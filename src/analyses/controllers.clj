e1fdade076218ceb432010e9715eafa419daefe3
John Wregglesworth <wregglej@iplantcollaborative.org>
2019-04-15 09:53:23 -0700

    Always validate paths for adding and updating quicklaunches

--- src/analyses/controllers.clj
+++ src/analyses/controllers.clj
@@ -4,16 +4,13 @@
 
 (defn add-quicklaunch
   [user quicklaunch]
-  (when (:is_public quicklaunch)
-    (clients/validate-submission (:submission quicklaunch) (clients/get-app user "de" (:app_id quicklaunch))))
+  (clients/validate-submission (:submission quicklaunch) (clients/get-app user "de" (:app_id quicklaunch)))
   (persist/add-quicklaunch user quicklaunch))
 
 (defn update-quicklaunch
   [id user quicklaunch]
-  (when (and (contains? quicklaunch :is_public)
-             (:is_public quicklaunch))
-    (let [app (clients/get-app user "de" (:app_id quicklaunch))]
-      (if (contains? quicklaunch :submission)
-        (clients/validate-submission (persist/merge-submission id user (:submission quicklaunch)) app)
-        (clients/validate-submission (persist/get-submission (:submission_id (persist/get-unjoined-quicklaunch id user))) app))))
-  (persist/update-quicklaunch id user quicklaunch))
+  (let [app (clients/get-app user "de" (:app_id quicklaunch))]
+    (if (contains? quicklaunch :submission)
+      (clients/validate-submission (persist/merge-submission id user (:submission quicklaunch)) app)
+      (clients/validate-submission (persist/get-submission (:submission_id (persist/get-unjoined-quicklaunch id user))) app))
+    (persist/update-quicklaunch id user quicklaunch)))
